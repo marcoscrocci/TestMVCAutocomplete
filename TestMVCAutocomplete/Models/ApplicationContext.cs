2bbf9445b16279a815c5602d33d5efe2eb2860f6
marcoscrocci@gmail.com <marcoscrocci@gmail.com>
2018-09-14 21:31:45 -0300

    Ajustes para gerar o banco de dados Code First

--- TestMVCAutocomplete/Models/ApplicationContext.cs
+++ TestMVCAutocomplete/Models/ApplicationContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -17,6 +18,33 @@ namespace TestMVCAutocomplete.Models
     
         public ApplicationContext() : base("name=ApplicationContext")
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Redefinindo o nome da tabela
+            modelBuilder.Entity<Brand>().ToTable("Brand");
+            modelBuilder.Entity<Car>().ToTable("Car");
+            modelBuilder.Entity<Color>().ToTable("Color");            
+
+            // Especificando a propriedade Id como chave primária.
+            modelBuilder.Entity<Brand>().HasKey(p => p.BrandId);
+            modelBuilder.Entity<Car>().HasKey(p => p.CarId);
+            modelBuilder.Entity<Color>().HasKey(p => p.ColorId);
+
+            // Especificando a chave Id como Identity (auto-incremento)
+            modelBuilder.Entity<Brand>()
+                .Property(p => p.BrandId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            modelBuilder.Entity<Car>()
+                .Property(p => p.CarId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            modelBuilder.Entity<Color>()
+                .Property(p => p.ColorId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
         }
 
         public System.Data.Entity.DbSet<TestMVCAutocomplete.Models.Brand> Brands { get; set; }
